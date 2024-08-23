bdde961dcc1b3804d5fcf0c622cdaa6eef4f9dad
Vishu Tyagi <tyagivishu277@gmail.com>
2024-08-23 18:38:23 +0530

    Final COMMIT

--- app/src/main/java/com/vishu/a7minutesworkout/HistoryDatabase.kt
+++ app/src/main/java/com/vishu/a7minutesworkout/HistoryDatabase.kt
@@ -8,6 +8,7 @@ import androidx.room.RoomDatabase
 @Database(entities = [HistoryEntity::class], version = 1)
 abstract class HistoryDatabase : RoomDatabase() {
 
+
     abstract fun historyDao(): HistoryDao
 
 
